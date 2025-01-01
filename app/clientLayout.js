790b49bd1a6ca04201d4a6fa90bf26aa20c77eb4
12.isk <66267884+12isk@users.noreply.github.com>
2025-01-01 16:21:22 +0100

    trying to fix scroll and menupreview issue

--- app/clientLayout.js
+++ app/clientLayout.js
@@ -4,13 +4,21 @@ import { ReactLenis } from "lenis/react";
 import Menu from "./components/menu";
 export default function ClientLayout({ children }) {
   const lenisOptions = {
-    lerp: 0.03,
-    duration: 0.85,
-    smoothTouch: true, // Enable smooth scroll for touch devices
-    smooth: true,
-    touchMultiplier: 2, // Adjust this value for touch sensitivity
-    wheelMultiplier: 1, // Adjust this for mouse wheel sensitivity
-    touchInertiaMultiplier: 2.5, // Controls inertia feeling on touch devices
+    lerp: 0.1,
+    duration: 1,
+    smoothTouch: true,
+    touchMultiplier: 2,
+    infinite: false,
+    gestureOrientation: "vertical",
+    orientation: "vertical",
+    smoothWheel: true,
+    wheelMultiplier: 1,
+    touchInertiaMultiplier: 2,
+    smooth: {
+      mouse: 0.75,
+      touch: 0.8,
+      wheel: 0.75,
+    },
   };
 
   return (
