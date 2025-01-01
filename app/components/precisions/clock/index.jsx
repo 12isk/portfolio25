52b07d328de84b5a57daa25cabc420b84a40be8b
12.isk <66267884+12isk@users.noreply.github.com>
2025-01-01 17:22:30 +0100

    added: 404 responsivity, clock disk

--- app/components/precisions/clock/index.jsx
+++ app/components/precisions/clock/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Clock from 'react-live-clock';
 import styles from './styles.module.scss';
 
+// JSX
 export default function index() {
     const colors = ["#3FFF00", "#00FFBF"]
     const size = 10;
@@ -10,26 +11,24 @@ export default function index() {
     <div className={styles.clockContainer}>
         <span className={styles.lv1}>Currently in Lyon<span className={styles.location}>, France</span></span>
         <div className={styles.wrap}>
-            <Clock className={styles.clock} format={'HH:mm:ss'} ticking={true} timezone={'Europe/Paris'} />
-        {/* <div className={styles.diskContainer}>
-            <div className={styles.diskWrapper}>
-                {colors.map((color, i) => {
-                    let newSize = size + i + 1;                    
-                    return (
-                        <div key={i} className={styles.disk}
-                         style={{ 
-                            backgroundColor: color,
-                            width: newSize,
-                            height: newSize}}>
-
-                        </div>
-                    );
-                })}
+            <div className={styles.diskContainer}>
+                <div className={styles.diskWrapper}>
+                    {colors.map((color, i) => {
+                        let newSize = size + i + 1;                    
+                        return (
+                            <div key={i} className={styles.disk}
+                             style={{ 
+                                backgroundColor: color,
+                                width: newSize,
+                                height: newSize}}>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
-        </div> */}
+            <Clock className={styles.clock} format={'HH:mm:ss'} ticking={true} timezone={'Europe/Paris'} />
+
         </div>
-        
-      
     </div>
   )
 }
