dc58f8c00fbb1e2772852eeb679843a80816fdcb
12.isk <66267884+12isk@users.noreply.github.com>
2025-01-20 22:34:07 +0100

    changed base roughness

--- app/playground/page.jsx
+++ app/playground/page.jsx
@@ -77,7 +77,7 @@ export default function ModelPlayground() {
                         rotation={[0,-Math.PI/2,0]}
                         onPointerOver={() => setIsHovered(true)} 
                         onPointerLeave={() => setIsHovered(false)}>
-                        oH yOu&#39;Re lOoKiNg around?
+                        oH yOu&#39;Re lOoKiNg around? Try ZoOMinG!
                     </Text>
                     <OrbitControls />
                     <Model
@@ -114,7 +114,7 @@ function Model({ isHovered, setIsHovered, onLoad }) {
         resolution: { value: 2048, min: 256, max: 2048, step: 256 },
         transmission: { value: 1, min: 0, max: 1 },
         roughness: { value: 0.0, min: 0, max: 1, step: 0.01 },
-        thickness: { value: 0.01, min: 0, max: 10, step: 0.01 },
+        thickness: { value: 0.05, min: 0, max: 10, step: 0.01 },
         ior: { value: 1, min: 1, max: 5, step: 0.01 },
         chromaticAberration: { value: 0.03, min: 0, max: 1 },
         anisotropy: { value: 0.42, min: 0, max: 1, step: 0.01 },
