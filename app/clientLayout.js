edc00c6617229371fcf44dae40e2a21d2fa6bd44
12.isk <66267884+12isk@users.noreply.github.com>
2025-02-23 22:53:16 +0100

    back to original scrolling logic

--- app/clientLayout.js
+++ app/clientLayout.js
@@ -7,21 +7,26 @@ export default function ClientLayout({ children }) {
   const isSlower = typeof window !== "undefined" && navigator.hardwareConcurrency < 6;
 
   const lenisOptions = {
-    lerp: isSlower ? 0.1 : 0.07,
-    duration: isSlower ? 0.65 : 1,
+    lerp: 0.03,
+    duration: isSlower ? 0.7 : 1,
     smoothTouch: true,
-    touchMultiplier: 1.1,
+    touchMultiplier: 1.2,
     infinite: false,
+    gestureOrientation: "vertical",
+    orientation: "vertical",
     smoothWheel: true,
-    // wheelMultiplier: isSlower ? 0.8 : 1,
-    onTouch: (e) => {
-      if (e.event?.target?.closest(".no-scroll")) {
-        e.event.preventDefault();
+    wheelMultiplier: 1,
+    //syncTouch: true,  
+    //touchInertiaMultiplier: 1.3,
+    breakpoints: {
+      tablet: {
+        smooth: true,
+        breakpoint: 1024,
+        touchMultiplier: 1.5,  // Slightly higher for tablets
+        duration: 0.8,  // Slightly faster for tablets
       }
-    },
+    }
   };
-
-  
 
   return (
     <ReactLenis root options={lenisOptions} suppressHydrationWarning>
